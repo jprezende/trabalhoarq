716fea479f49a6aab1b5841292aafd90cb898685
usuario <jpeluzio@hotmail.com>
2017-07-05 18:30:17 -0300

    PG implementada com LSL, processador não tem a função implementada, testado no visual arm.

---
+++ pgcomlsl.s
@@ -0,0 +1,21 @@
+MAIN:
+		AND		R0,R0,#0
+		ADD		R4,R0,#2
+		ADD		R5,R0,#0x010000
+		ADD		R6,R0,#100
+		AND		R8,R8,#0
+		AND		R7,R7,#0
+		STR		R4,[R5,r6]
+LOOP:
+		CMP		R8,#9
+		BGE		FIM
+		TST		R4,#2147483648
+		ADD		R8,R8,#1
+		LSL		R7,R4,#2
+		ADD		R6,R6,#4
+		ADD		R4,R7,#0
+		STR		R4,[R5,r6]
+		B		LOOP
+FIM:
+		LDR		R1,[R5,#0x088]
+		END
--- pgteste.s
+++ pgteste.s
@@ -1,23 +1,21 @@
 MAIN:				
-		AND		R0,R0,#0 	;seta r0 como referencia
-		ADD		R4,R0,#2 	;primeiro termo da pg
+		AND		R0,R0,#0 	
+		ADD		R4,R0,#2 	
 		ADD		R5,R0,#0
-		ADD		R6,R0,#100 	;seleciona endereço de memoria inicial
-		AND		R8,R8,#0	;inicializa contador em 0
+		ADD		R6,R0,#100 	
+		AND		R8,R8,#0	
 		AND		R7,R7,#0
-		STR		R4,[R6,#0]	;grava primeiro termo da pg no endereço 100
+		STR		R4,[R6,#0]	
 LOOP:
 		CMP		R8,#9
 		BGE		FIM
 		TST		R4,#2147483648		
-		ADD		R8,R8,#1	;incrementa contador
-		ADD		R7,R4,R4	;multiplica por 2
-		ADD		R6,R6,#4	;pula para proximo endereço de memoria
-		ADD		R4,R7,#0	;move termo da pg para r4		
-		STR		R4,[R6,#0]	;armazena termos no endereço de memoria
+		ADD		R8,R8,#1	
+		ADD		R7,R4,R4	
+		ADD		R6,R6,#4	
+		ADD		R4,R7,#0			
+		STR		R4,[R6,#0]	
 		B		LOOP
-		LDR		R1,[R0,#136] 	;armazena em r1 ultimo termo da pg
-
 FIM:
-
-
+		LDR		R1,[R0,#136] 
+END:
